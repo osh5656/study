bef68e0ecbec3c1b388c9a16aac4783337ae60af
shoh <osh5656@naver.com>
2023-04-16 17:13:35 +0900

    작업내용 : 백준 gitignore 추가 작업일시 : 2023.04.16 작업자 : 오세현

---
+++ algorithm/BackJoon/src/A더하기B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class A더하기B {
+
+    /**
+     * 문제 - https://www.acmicpc.net/problem/1000
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
