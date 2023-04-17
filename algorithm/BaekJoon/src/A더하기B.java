404f4f2dfd53293c8eed82d0cdd394a8cb5a9cab
shoh <osh5656@naver.com>
2023-04-17 10:12:25 +0900

    작업내용 : BaekJoon 디렉토리 추가 작업일시 : 2023.04.17 작업자 : 오세현

---
+++ algorithm/BaekJoon/src/A더하기B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * https://www.acmicpc.net/problem/1000
+ */
+public class A더하기B {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
