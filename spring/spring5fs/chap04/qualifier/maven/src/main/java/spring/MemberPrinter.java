65af6cf78083755b52de39808910b15c79abd3ee
OhSeHyeon <osh5656@gmail.com>
2023-09-18 21:07:00 +0900

    작업내용 : 스프링 5 프로그래밍 입문 chap 04 - 5

--- spring/spring5fs/chap04/qualifier/maven/src/main/java/spring/MemberPrinter.java
+++ spring/spring5fs/chap04/qualifier/maven/src/main/java/spring/MemberPrinter.java
@@ -1,13 +1,25 @@
 package spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 public class MemberPrinter {
-    private DateTimeFormatter dateRimeDateTimeFormatter;
+//    @Autowired(required = false)
+//    private DateTimeFormatter dateTimeFormatter;
+
+    @Autowired
+    private Optional<DateTimeFormatter> formatterOpt;
+
+//    @Autowired
+//    @Nullable
+//    private DateTimeFormatter dateTimeFormatter;
+
     public void print(Member member) {
-        if(dateRimeDateTimeFormatter == null) {
+        DateTimeFormatter dateTimeFormatter = formatterOpt.orElse(null);
+        if(dateTimeFormatter == null) {
             System.out.printf(
                     "회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n",
                     member.getId(), member.getEmail(),
@@ -18,13 +30,27 @@ public class MemberPrinter {
                     "회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%s\n",
                     member.getId(), member.getEmail(),
                     member.getName(),
-                    dateRimeDateTimeFormatter.format(member.getRegisterDateTime())
+                    dateTimeFormatter.format(member.getRegisterDateTime())
             );
         }
     }
 
-    @Autowired(required = false)
-    public void setDateRimeDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
-        this.dateRimeDateTimeFormatter = dateTimeFormatter;
-    }
+//    @Autowired(required = false)
+//    public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+//        this.dateTimeFormatter = dateTimeFormatter;
+//    }
+
+//    @Autowired
+//    public void setDateFormatter(Optional<DateTimeFormatter> formatterOpt) {
+//        if(formatterOpt.isPresent()) {
+//            this.dateTimeFormatter = formatterOpt.get();
+//        } else {
+//            this.dateTimeFormatter = null;
+//        }
+//    }
+
+//    @Autowired
+//    public void setDateFormatter(@Nullable DateTimeFormatter dateTimeFormatter) {
+//        this.dateTimeFormatter = dateTimeFormatter;
+//    }
 }
